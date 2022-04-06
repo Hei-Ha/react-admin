d336d8a566c0bc6c399d9867c82d98d9b362d8c5
wangchuang <wangchuang.space@gmail.com>
2022-04-06 23:05:17 +0800

    添加 ant design

--- build/webpack.config.common.js
+++ build/webpack.config.common.js
@@ -5,15 +5,13 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: '[name].js',
-        // filename: 'main.js',
-        // __dirname: 当前模块的目录名
         path: path.resolve(process.cwd(), 'dist'),  // path.resolve 将给定路径解析为绝对路径
         clean: true
     },
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: path.resolve(process.cwd(), '../public/index.html'),
+            template: path.resolve(process.cwd(), './public/index.html'),
             inject: 'body',
             minify: {
                 removeComments: true,
@@ -29,24 +27,24 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                type: 'url-loader',
-                options: {
-                    name: '[name].[ext]', // 输出的文件名为 原来的文件名
-                    outputPath: 'images',
-                    limit: 8192 // 指定文件的最大体积（以字节为单位）
-                }
-            },
             // {
             //     test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-            //     type: 'asset',
-            //     parser: {
-            //         dataUrlCondition: {
-            //             maxSize: 12000, // 图片超过这个大小，使用原图片，否则使用base64字符串
-            //         }
+            //     type: 'url-loader',
+            //     options: {
+            //         name: '[name].[ext]', // 输出的文件名为 原来的文件名
+            //         outputPath: 'images',
+            //         limit: 8192 // 指定文件的最大体积（以字节为单位）
             //     }
             // },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 12000, // 图片超过这个大小，使用原图片，否则使用base64字符串
+                    }
+                }
+            },
             {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
