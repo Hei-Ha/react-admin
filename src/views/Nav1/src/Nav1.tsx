21abe532fb1cf35fb890c1593e6dd6aba6c3e9dd
wangchuang <wangchuang.space@gmail.com>
2022-04-24 18:53:16 +0800

    完善

--- src/views/Nav1/src/Nav1.tsx
+++ src/views/Nav1/src/Nav1.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Card, Form, Input } from 'antd'
+import { Card, Form, Input, Table } from 'antd'
 
 
 interface FormValues {
     name: string;
+    id: number;
 }
 export const Nav1 = () => {
     const [form] = Form.useForm<FormValues>()
@@ -11,14 +12,18 @@ export const Nav1 = () => {
     return <>
         <Card>
             <Form
+                layout={'inline'}
                 form={form}>
-                <Form.Item name={'name'} className={'formItem-width-120'}>
-                    <Input />
+                <Form.Item name={'name'} className={'formItem-width-180'}>
+                    <Input placeholder={'请输入姓名'} />
+                </Form.Item>
+                <Form.Item name={'id'} className={'formItem-width-150'}>
+                    <Input placeholder={'请输入 id'} />
                 </Form.Item>
             </Form>
         </Card>
-        <Card>
-            table
+        <Card style={{ marginTop: '20px'}}>
+            <Table />
         </Card>
     </>
 }
