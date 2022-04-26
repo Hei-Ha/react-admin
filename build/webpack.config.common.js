31a96b6a074a4a27d0b1366ea96782c34f415ef8
wangchuang <wangchuang.space@gmail.com>
2022-04-26 16:31:54 +0800

    添加下拉框

--- build/webpack.config.common.js
+++ build/webpack.config.common.js
@@ -53,7 +53,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.tsx$/,
+                test: /\.(tsx|ts)$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
             }
--- src/views/Nav1/src/Nav1.tsx
+++ src/views/Nav1/src/Nav1.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card, Form, Input, Table } from 'antd'
+import { Card, Form, Input, Table, Select, Button } from 'antd'
+import { ID } from '@/common/constants'
 
 
 interface FormValues {
@@ -8,6 +9,10 @@ interface FormValues {
 }
 export const Nav1 = () => {
     const [form] = Form.useForm<FormValues>()
+
+    const headSearch = () => {
+        console.log(form.getFieldsValue())
+    }
 
     return <>
         <Card>
@@ -18,7 +23,15 @@ export const Nav1 = () => {
                     <Input placeholder={'请输入姓名'} />
                 </Form.Item>
                 <Form.Item name={'id'} className={'formItem-width-150'}>
-                    <Input placeholder={'请输入 id'} />
+                    <Select placeholder={'请选择 ID'}>
+                        {ID.map((item) => {
+                            return <Select.Option value={item.value} title={item.label} key={item.value + item.label}>{item.label}</Select.Option>
+                        })}
+                    </Select>
+                </Form.Item>
+                <Form.Item>
+                    <Button type={'primary'} onClick={() => { headSearch() }}>查询</Button>
+                    <Button style={{ marginLeft: '10px'}}>重置</Button>
                 </Form.Item>
             </Form>
         </Card>
