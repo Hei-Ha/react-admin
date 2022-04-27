4238e3146d14d20cc4fce88e6658ae5a717a5a49
wangchuang <wangchuang.space@gmail.com>
2022-04-27 23:19:44 +0800

    test ant design

--- src/views/Nav1/src/Nav1.tsx
+++ src/views/Nav1/src/Nav1.tsx
@@ -14,6 +14,52 @@ export const Nav1 = () => {
         console.log(form.getFieldsValue())
     }
 
+    const tableData = [
+        {
+            key: '1',
+            name: 'John Brown',
+            age: 32,
+            address: 'New York No. 1 Lake Park',
+            tags: ['nice', 'developer'],
+        },
+        {
+            key: '2',
+            name: 'Jim Green',
+            age: 42,
+            address: 'London No. 1 Lake Park',
+            tags: ['loser'],
+        },
+        {
+            key: '3',
+            name: 'Joe Black',
+            age: 32,
+            address: 'Sidney No. 1 Lake Park',
+            tags: ['cool', 'teacher'],
+        }
+    ]
+
+    const columns= [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            fixed: true,
+            key: 'name',
+            width: 400,
+            render: (text: string) => <a>{text}</a>,
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            width: 500,
+            key: 'age',
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            width: 1500,
+            key: 'address',
+        }
+    ]
     return <>
         <Card>
             <Form
@@ -26,7 +72,7 @@ export const Nav1 = () => {
                     <Select placeholder={'请选择 ID'}>
                         {ID.map((item) => {
                             return <Select.Option value={item.value} title={item.label} key={item.value + item.label}>{item.label}</Select.Option>
-                        })}
+                        })}å
                     </Select>
                 </Form.Item>
                 <Form.Item>
@@ -35,8 +81,15 @@ export const Nav1 = () => {
                 </Form.Item>
             </Form>
         </Card>
-        <Card style={{ marginTop: '20px'}}>
-            <Table />
-        </Card>
+        <div className={'self-body'}>
+            <Table
+                className={'selfTable'}
+                dataSource={tableData}
+                columns={columns}
+                pagination={{
+                    total: 100
+                }}
+            />
+        </div>
     </>
 }
