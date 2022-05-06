8c497e79b4840f0906d713053774e84fef7f94f5
wangchuang <wangchuang.space@gmail.com>
2022-05-06 19:22:48 +0800

    service rc-1

--- src/components/http/index.ts
+++ src/components/http/index.ts
@@ -1,4 +1,4 @@
-import Axios from 'axios'
+import Axios, { AxiosRequestConfig } from 'axios'
 import envConfig from 'build/env.config'
 
 const HTTP = Axios.create({
@@ -9,6 +9,19 @@ const HTTP = Axios.create({
 
 // 请求拦截器
 HTTP.interceptors.request.use((config) => {
-    // do someting
+    // do someting （携带 token）
     return config
 })
+
+HTTP.interceptors.response.use((response) => {
+    // 2xx 范围内的状态码都会触发该函数。
+    return response
+}, () => {
+    // 超出 2xx 范围的状态码都会触发该函数。
+})
+
+export const GET = (url: string, params: object) => {
+    return HTTP.get(url, {
+        params
+    })
+}
