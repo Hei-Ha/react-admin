97d1333a84de6583ed0f82b5bb9dd5eda39005e4
wangchuang_MBP_16 <wangchuang.space@gmail.com>
2022-06-18 12:04:26 +0800

    添加 test Page ，测试代码用

---
+++ src/views/testPage/menu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {BlockOutlined} from "@ant-design/icons";
+
+export default [
+    {
+        label: 'TestPage',
+        path: '/testPage',
+        rules: ['testPage'],
+        icon: <BlockOutlined />,
+        weight: 2
+    }
+]
---
+++ src/views/testPage/router.js
@@ -0,0 +1,11 @@
+import { TestPage } from './src/testPage'
+import React from "react";
+
+export default [
+    {
+        name: 'testPage',
+        path: '/testPage',
+        element: <TestPage />,
+        rules: ['testPage']
+    }
+]
